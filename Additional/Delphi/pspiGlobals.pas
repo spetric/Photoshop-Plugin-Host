65c656c510332c9dffa90f5bf5c1d54564b10b66
Sinisa Petric <sinisa.petric@yahoo.com>
2020-08-13 10:58:49 +0200

    Updated pas files (headers)
    Pas files updated.

--- Additional/Delphi/pspiGlobals.pas
+++ Additional/Delphi/pspiGlobals.pas
@@ -1,36 +1,38 @@
-unit pspiGlobals;
+﻿unit pspiGlobals;
 
 interface
 
 uses
   Windows;
 
-// constants
+// Result values
 const PSPI_ERR_FILTER_NOT_LOADED   = 1;
 const PSPI_ERR_FILTER_BAD_PROC     = 2;
 const PSPI_ERR_FILTER_ABOUT_ERROR  = 3;
 const PSPI_ERR_FILTER_DUMMY_PROC   = 4;
-const PSPI_ERR_FILTER_CANCELED	    = 5;
-const PSPI_ERR_FILTER_CRASHED	    = 6;
-const PSPI_ERR_FILTER_INVALID	    = 7;
-const PSPI_ERR_IMAGE_INVALID	    = 10;
-const PSPI_ERR_MEMORY_ALLOC	    = 11;
-const PSPI_ERR_INIT_PATH_EMPTY	    = 12;
+const PSPI_ERR_FILTER_CANCELED     = 5;
+const PSPI_ERR_FILTER_CRASHED      = 6;
+const PSPI_ERR_FILTER_INVALID      = 7;
+const PSPI_ERR_IMAGE_INVALID       = 10;
+const PSPI_ERR_MEMORY_ALLOC        = 11;
+const PSPI_ERR_INIT_PATH_EMPTY     = 12;
 const PSPI_ERR_WORK_PATH_EMPTY     = 13;
-const PSPI_ERR_BAD_PARAM	    = 14;
-const PSPI_ERR_BAD_IMAGE_TYPE		= 15;
+const PSPI_ERR_BAD_PARAM           = 14;
+const PSPI_ERR_BAD_IMAGE_TYPE      = 15;
 
 // enums
+{$MINENUMSIZE 4}
 type TImgType = (PSPI_IMG_TYPE_BGR = 0, PSPI_IMG_TYPE_BGRA, PSPI_IMG_TYPE_RGB, PSPI_IMG_TYPE_RGBA, PSPI_IMG_TYPE_GRAY, PSPI_IMG_TYPE_GRAYA);
-type TImgOrientation (PSPI_IMG_ORIENTATION_ASIS = 0, PSPI_IMG_ORIENTATION_INVERT);
+type TImgOrientation = (PSPI_IMG_ORIENTATION_ASIS = 0, PSPI_IMG_ORIENTATION_INVERT);
 
 // typedefs
-type 
-   ENUMCALLBACK = procedure(p1: PAnsiChar; p2: PAnsiChar; p3: PAnsiChar; p4: PWideChar); cdecl;
-   PROGRESSCALLBACK = procedure(p1: DWORD; p2: DWORD); cdecl;
-   COLORPICKERCALLBACK = function(var p1: DWORD): LongBool; cdecl;
+type
+   TENUMCALLBACK = procedure(p1: PAnsiChar; p2: PAnsiChar; p3: PAnsiChar; p4: PWideChar); cdecl;
+   TPROGRESSCALLBACK = procedure(p1: DWORD; p2: DWORD); cdecl;
+   TCOLORPICKERCALLBACK = function(var p1: DWORD): LongBool; cdecl;
 
 
 implementation
 
 end.
+
--- Additional/Delphi/pspiHost.pas
+++ Additional/Delphi/pspiHost.pas
@@ -4,20 +4,21 @@ interface
 
 uses pspiGlobals, Windows;
 
-          
+
 type
   int = Integer;
 
 // get psipHost version
-function pspiGetVersion(): PAnsiChar cdecl; 
+function pspiGetVersion(): PAnsiChar cdecl;
 
 // set path
 function pspiSetPath(filterFolder: Pointer): int cdecl;
 
 // set roi
 function pspiSetRoi(top: int = 0; left: int = 0; bottom: int = 0; right: int = 0): int  cdecl;
+
 // set image orientation
-function pspiSetImageOrientation(orientation: TImgOrientation) int cdecl;
+function pspiSetImageOrientation(orientation: TImgOrientation): int cdecl;
 
 // set image using contiguous memory buffer pointer
 // note: source image is shared - do not destroy source image in your host program before plug-in is executed
@@ -25,7 +26,7 @@ function pspiSetImage(imgtype: TImgType; width: int; height: int; imageBuff: Poi
 
 // set mask using contiguous memory buffer pointer
 // note: source mask is shared - do not destroy source maske in your host program before plug-in is executed
-function pspiSetMask(width: int; height: int; maskBuff: Pointer; maskStride: int;  useMaskByPi: Boolean = true): int cdecl;
+function pspiSetMask(width: int = 0; height : int = 0; maskBuff: Pointer = nil; maskStride: int = 0;  useMaskByPi: Boolean = true): int cdecl;
 
 // block for adding image scanlines (possibly non-contiguous image)
 // note: source image is shared - do not destroy source image in your host program before plug-in is executed
@@ -39,39 +40,40 @@ function pspiStartMaskSL(width: int; height: int; useMaskByPi: Boolean = true): 
 function pspiAddMaskSL(maskScanLine: Pointer): int cdecl;
 function pspiFinishMaskSL(maskStride: int = 0): int cdecl;
 
-// release all images - all image buffers including mask are released when application is closed; but sometimes it's necessary to free memory (big images) 
+// release all images - all image buffers including mask are released when application is closed; but sometimes it's necessary to free memory (big images)
 function pspiReleaseAllImages(): int cdecl;
 
 // set call backs of interset
-function pspiSetProgressCallBack(progressProc: PROGRESSCALLBACK): int cdecl;
-function pspiSetColorPickerCallBack(colorPickerProc: COLORPICKERCALLBACK): int cdecl;
+function pspiSetProgressCallBack(progressProc: TPROGRESSCALLBACK): int cdecl;
+function pspiSetColorPickerCallBack(colorPickerProc: TCOLORPICKERCALLBACK): int cdecl;
 
-// plug-in related 
+// plug-in related
 function pspiPlugInLoad(filter: Pointer): int cdecl;
 function pspiPlugInAbout(hWnd: HWND = 0): int cdecl;
 function pspiPlugInExecute(hWnd: HWND = 0): int cdecl;
-function pspiPlugInEnumerate(enumFunc: ENUMCALLBACK; recurseSubFolders: Boolean = true): int cdecl;
+function pspiPlugInEnumerate(enumFunc: TENUMCALLBACK; recurseSubFolders: Boolean = true): int cdecl;
 
 implementation
 
-function pspiGetVersion(); external 'pspiHost.dll';
-function pspiSetPath(filterFolder: Pointer); external 'pspiHost.dll';
-function pspiSetRoi(top: int = 0; left: int = 0; bottom: int = 0; right: int = 0); external 'pspiHost.dll';
-function pspiSetImageOrientation(orientation: TImgOrientation) external 'pspiHost.dll';
-function pspiSetImage(imgtype: TImgType; width: int; height: int; imageBuff: Pointer; imageStride: int; alphaBuff: Pointer = nil; alphaStride: int = 0); external 'pspiHost.dll';
-function pspiSetMask(width: int; height: int; maskBuff: Pointer; maskStride: int;  useMaskByPi: Boolean = true); external 'pspiHost.dll';
-function pspiStartImageSL(imgtype: TImgType; width: int; height: int; externalAlpha: Boolean = false); external 'pspiHost.dll';
-function pspiAddImageSL(imageScanLine: Pointer; alphaScanLine: Pointer = nil); external 'pspiHost.dll';
-function pspiFinishImageSL(imageStride: int = 0; alphaStride: int = 0); external 'pspiHost.dll';
-function pspiStartMaskSL(width: int; height: int; useMaskByPi: Boolean = true); external 'pspiHost.dll';
-function pspiAddMaskSL(maskScanLine: Pointer); external 'pspiHost.dll';
-function pspiFinishMaskSL(maskStride: int = 0); external 'pspiHost.dll';
-function pspiReleaseAllImages(); external 'pspiHost.dll';
-function pspiSetProgressCallBack(progressProc: PROGRESSCALLBACK); external 'pspiHost.dll';
-function pspiSetColorPickerCallBack(colorPickerProc: COLORPICKERCALLBACK); external 'pspiHost.dll';
-function pspiPlugInLoad(filter: Pointer); external 'pspiHost.dll';
-function pspiPlugInAbout(hWnd: HWND = 0); external 'pspiHost.dll';
-function pspiPlugInExecute(hWnd: HWND = 0); external 'pspiHost.dll';
-function pspiPlugInEnumerate(enumFunc: ENUMCALLBACK; recurseSubFolders: Boolean = true); external 'pspiHost.dll';
+function pspiGetVersion(): PAnsiChar; external 'pspiHost.dll';
+function pspiSetPath(filterFolder: Pointer): int; external 'pspiHost.dll';
+function pspiSetRoi(top: int = 0; left: int = 0; bottom: int = 0; right: int = 0): int; external 'pspiHost.dll';
+function pspiSetImageOrientation(orientation: TImgOrientation): int; external 'pspiHost.dll';
+function pspiSetImage(imgtype: TImgType; width: int; height: int; imageBuff: Pointer; imageStride: int; alphaBuff: Pointer = nil; alphaStride: int = 0): int; external 'pspiHost.dll';
+function pspiSetMask(width: int = 0; height: int = 0; maskBuff: Pointer = nil; maskStride: int = 0;  useMaskByPi: Boolean = true): int; external 'pspiHost.dll';
+function pspiStartImageSL(imgtype: TImgType; width: int; height: int; externalAlpha: Boolean = false): int; external 'pspiHost.dll';
+function pspiAddImageSL(imageScanLine: Pointer; alphaScanLine: Pointer = nil): int; external 'pspiHost.dll';
+function pspiFinishImageSL(imageStride: int = 0; alphaStride: int = 0): int; external 'pspiHost.dll';
+function pspiStartMaskSL(width: int; height: int; useMaskByPi: Boolean = true): int; external 'pspiHost.dll';
+function pspiAddMaskSL(maskScanLine: Pointer): int; external 'pspiHost.dll';
+function pspiFinishMaskSL(maskStride: int = 0): int; external 'pspiHost.dll';
+function pspiReleaseAllImages(): int; external 'pspiHost.dll';
+function pspiSetProgressCallBack(progressProc: TPROGRESSCALLBACK): int; external 'pspiHost.dll';
+function pspiSetColorPickerCallBack(colorPickerProc: TCOLORPICKERCALLBACK): int; external 'pspiHost.dll';
+function pspiPlugInLoad(filter: Pointer): int; external 'pspiHost.dll';
+function pspiPlugInAbout(hWnd: HWND = 0): int; external 'pspiHost.dll';
+function pspiPlugInExecute(hWnd: HWND = 0): int; external 'pspiHost.dll';
+function pspiPlugInEnumerate(enumFunc: TENUMCALLBACK; recurseSubFolders: Boolean = true): int; external 'pspiHost.dll';
 
 end.
+
